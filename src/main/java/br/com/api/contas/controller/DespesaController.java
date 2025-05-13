4afdfcc286e349becc0161298450aeac7be9c9e7
JoaoIkko <mrpinote1@gmail.com>
2025-05-13 19:41:00 -0300

    postgrsqlabcdefg

--- src/main/java/br/com/api/contas/controller/DespesaController.java
+++ src/main/java/br/com/api/contas/controller/DespesaController.java
@@ -27,7 +27,7 @@ import br.com.api.contas.service.SaldoService;
 
 @RestController
 @RequestMapping("/despesas")
-@CrossOrigin(origins = "https://paineldespesas.up.railway.app")
+//@CrossOrigin(origins = "https://paineldespesas.up.railway.app")
 public class DespesaController {
 
 private final DespesaService despesaService;
